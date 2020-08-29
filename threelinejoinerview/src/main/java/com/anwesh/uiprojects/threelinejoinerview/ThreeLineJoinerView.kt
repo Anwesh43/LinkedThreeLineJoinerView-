88eedfb8e1733497944e9ace00d8ed9cc1b06b89
Anwesh43 <anweshthecool0@gmail.com>
2020-08-30 02:32:10 +0530

    adding Canvas extension method

--- threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
+++ threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
@@ -30,3 +30,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))* n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Int.midExpand(n : Int) : Int = (n + 1) / 2 - Math.abs(this - (lines / 2))
+
+fun Canvas.drawMultiJoinerLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, lines)
+    val gap : Float = w / lines
+    val hGap : Float = h / ((lines + 1) / 2 * hFactor)
+    for (j in 0..(lines - 1)) {
+        val i : Int = j.midExpand(lines)
+        val yCurr : Float = -hGap * i * sf1
+        save()
+        translate(gap / 2 + gap * i, h)
+        drawLine(0f, 0f, 0f, yCurr, paint)
+        if (i != lines - 1) {
+            val sfj : Float = sf.divideScale(j + 1, lines)
+            val iNext : Int = (j + 1).midExpand(lines)
+            val y : Float = -hGap * iNext
+            drawLine(0f, yCurr, gap * sfj, yCurr + (y - yCurr) * sfj, paint)
+        }
+        restore()
+    }
+}
+
+fun Canvas.drawMJLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMultiJoinerLine(scale, w, h, paint)
+}
