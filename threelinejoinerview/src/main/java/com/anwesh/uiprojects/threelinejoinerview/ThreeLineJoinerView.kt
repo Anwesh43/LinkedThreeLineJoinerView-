5dc426d02e36828fad083c41b0921dc68c4b7a24
Anwesh43 <anweshthecool0@gmail.com>
2020-08-30 02:58:53 +0530

    adding Renderer class

--- threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
+++ threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
@@ -187,4 +187,27 @@ class ThreeLineJoinerView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ThreeLineJoinerView) {
+
+        private val animator : Animator = Animator(view)
+        private val tlj : ThreeLineJoiner = ThreeLineJoiner(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tlj.draw(canvas, paint)
+            animator.animate {
+                tlj.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tlj.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
