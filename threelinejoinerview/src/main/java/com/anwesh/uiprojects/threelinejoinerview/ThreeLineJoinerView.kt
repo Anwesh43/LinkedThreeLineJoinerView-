87ab1b26809dbb2deb72b0949516c924201d079f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-30 02:39:40 +0530

    adding Animator class

--- threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
+++ threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
@@ -96,4 +96,31 @@ class ThreeLineJoinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
