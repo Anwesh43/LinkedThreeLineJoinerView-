c92b46eee45b3bb482c099a48356960fe44d1eb6
Anwesh43 <anweshthecool0@gmail.com>
2020-08-30 02:43:10 +0530

    adding TLJNode class

--- threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
+++ threelinejoinerview/src/main/java/com/anwesh/uiprojects/threelinejoinerview/ThreeLineJoinerView.kt
@@ -123,4 +123,45 @@ class ThreeLineJoinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLJNode(var i : Int, val state : State = State()) {
+
+        private var next : TLJNode? = null
+        private var prev : TLJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMJLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLJNode {
+            var curr : TLJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
